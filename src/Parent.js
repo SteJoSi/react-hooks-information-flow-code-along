8aea82fd14f4a43badabf514889ff89b32d98381
Stephanie Sison <sjsison27@icloud.com>
2022-08-24 11:51:58 -0700

    commit tree 00c04416be1bbb5cc9e4ae43f6fbad91be293e95

--- src/Parent.js
+++ src/Parent.js
@@ -3,13 +3,20 @@ import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
 function Parent() {
+  //initial value for color state
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
 
+  //update color state to a new value
+  function handleChangeColor() {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+  }
+
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child onChangeColor={handleChangeColor} />
+      <Child onChangeColor={handleChangeColor} />
     </div>
   );
 }
